f8b642da205fa8023ec5e97dc7eea6e220eb8f35
Aman Pandey <69633924+Amanpandey046@users.noreply.github.com>
2021-12-18 13:38:50 +0530

    Update F_P1

--- Scripting/F_P1.py
+++ Scripting/F_P1.py
@@ -1,4 +1,12 @@
-f = open("F_1.txt")
-content = f.read()
-print(content)
-f.close()
+#Opening File OR We Say Read The File.
+
+with open("F_1.txt",'r',encoding = "UTF-8") as file:
+    content = file.read()
+
+#After Read The File I Want to Repalce the "India" TO "America".
+
+content = content.replace('America','India') 
+
+#Write The File After the Changing.
+with open("F_1.txt",'w',encoding = "UTF-8") as file:
+    file.write(content)

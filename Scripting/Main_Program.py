1870744dfec4f6422c26589e9fbbe5849f46027d
Aman Pandey <69633924+Amanpandey046@users.noreply.github.com>
2022-01-02 14:49:58 +0530

    Main File
    Basically It was like pick up the word from Firstfile and add it to Secondfile.

---
+++ Scripting/Main_Program.py
@@ -0,0 +1,8 @@
+# open both files
+with open('Check.txt','r') as firstfile, open('Basics.txt','a') as secondfile:
+      
+    # read content from first file
+    for line in firstfile:
+               
+             # append content to second file
+             secondfile.write("Aman Pandey ")

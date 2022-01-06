44477aafe879c86a83849c779602dfaf7a60ee44
Gaurang Pandey <86218103+gaurangp130693@users.noreply.github.com>
2022-01-06 16:03:21 +0530

    Updated the logic to pick the correct line.

--- Scripting/Main_Program.py
+++ Scripting/Main_Program.py
@@ -1,8 +1,9 @@
 # open both files
-with open('Check.txt','r') as firstfile, open('Basics.txt','a') as secondfile:
-      
+with open('Check.txt','r') as firstfile, open('Basics.txt','w') as secondfile:
+
     # read content from first file
     for line in firstfile:
-               
+       for word in line.split():
+          if word == 'WhatsApp':
              # append content to second file
-             secondfile.write("WhatsApp ")
+             secondfile.write(line)
